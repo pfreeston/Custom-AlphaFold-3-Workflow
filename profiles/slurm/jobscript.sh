5d760726e1b2af4241c550ac4f36ec836485517c
Phoebe <phoebe.freeston@biology.ox.ac.uk>
2026-04-27 17:28:50 +0100

    changing pipeline.smk to Snakefile and adjusting jobscript.sh

--- profiles/slurm/jobscript.sh
+++ profiles/slurm/jobscript.sh
@@ -2,11 +2,11 @@
 # profiles/slurm/jobscript.sh
 
 # Load required modules
-module load Anaconda3
+module load Anaconda3/2025.06-1
 module load Singularity
 
 # Activate your environment
-conda activate af3-workflow
+conda activate af3-custom-workflow
 
 # Run Snakemake job
 {exec_job}
