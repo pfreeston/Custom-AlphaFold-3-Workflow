b9344429f65433b2a7145fe94d16693fde890fc0
Phoebe <phoebe.freeston@biology.ox.ac.uk>
2026-04-27 17:36:22 +0100

    changing jobscript to include python path

--- profiles/slurm/jobscript.sh
+++ profiles/slurm/jobscript.sh
@@ -5,6 +5,10 @@
 module load Anaconda3/2025.06-1
 module load Singularity
 
+# Python path
+
+export PYTHONPATH=/data/dops-plant-genomics/lady8018/af3/Custom-AlphaFold-3-Workflow
+
 # Activate your environment
 conda activate af3-custom-workflow
 
